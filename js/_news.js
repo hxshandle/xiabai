5893e7ce027782bb9d866ceb498e5b7e11576751
hxshandle <handle.huang0409@gmail.com>
2014-06-24 22:20:26 +0800

    fix news

---
+++ js/_news.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('#c4-1').parallax(0, 0.3, 1800);
+  $('#c4-2').parallax(0, 0.2, 2500, true);
+  $('#c4-3').parallax(0, 0.4, 1300);
+
+
+  $('#news .news-entry:first').addClass('active');
+});
