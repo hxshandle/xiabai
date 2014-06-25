cc3fb23f5cea59a001f5079abb9cfc5426f139e8
handle <handle@linux-ecdev.dhcp.wdf.sap.corp>
2014-06-25 14:38:19 +0800

    ab

--- js/_news.js
+++ js/_news.js
@@ -1,8 +1,26 @@
-$(function(){
+$(function() {
   $('#c4-1').parallax(0, 0.3, 1800);
   $('#c4-2').parallax(0, 0.2, 2500, true);
   $('#c4-3').parallax(0, 0.4, 1300);
 
-
   $('#news .news-entry:first').addClass('active');
+
+  var totalNewsCount = $('.news-entry').length;
+  var currentNewsIndex = 0;
+
+  $('#new-pre').click(function() {
+    if (currentNewsIndex == 0) {
+      return;
+    }
+
+  });
+
+  $('#new-next').click(function() {
+    if (currentNewsIndex == totalNewsCount - 1) {
+      return;
+    }
+
+  });
+
 });
+
